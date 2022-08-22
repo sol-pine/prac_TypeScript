157ae519d2d278f826a72ddd57dcc06b80d0b76f
sol <osunpine@gmail.com>
2022-08-22 17:16:19 +0900

    feat. useState 타입 지정, props 타입 지정

--- react-type/src/App.tsx
+++ react-type/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+let box: JSX.Element = <div></div>;
 
 function App() {
+  // useState 타입은 자동화
+  let [user, setUser] = useState("Jake");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile name="John" age="20"></Profile>
     </div>
   );
+}
+// props 타입 지정 잘하면 버그 줄일 수 있음
+function Profile(props: { name: string; age: string }): JSX.Element {
+  return <div>{props.name}입니다</div>;
 }
 
 export default App;
